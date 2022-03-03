4bf3674d1ee2a9d1363fbcec9a2aa959585ede5b
Taylor Griffin <tmgriffin73@gmail.com>
2022-03-02 22:35:34 -0500

    add blog list on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,35 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx {
+        nodes {
+          frontmatter {
+            date(fromNow: true)
+            title
+            description
+          }
+          id
+          slug
+        }
+      }
+    }
+  `);
+  const posts = data.allMdx.nodes;
   return (
     <Layout>
       <h1>Hello Frontend Masters!</h1>
       <Link to="/about">Learn About</Link>
+      <h2>Check out my recent blog posts</h2>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+          <small>posted {post.frontmatter.date}</small>
+        </li>
+      ))}
     </Layout>
   );
 }
