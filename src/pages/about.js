8b23d97782dbb233c1a073b3534087cfd8938b2d
Taylor Griffin <tmgriffin73@gmail.com>
2022-03-04 13:03:26 -0500

    add images to mdx posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+export default function AboutPage({ data }) {
   return (
-    <Layout>
+    <Layout title="About this Site">
+      <GatsbyImage image={getImage(data.file)} alt="an elaborate cocktail " />
       <h1>About this site</h1>
       <Link to="/">Back to home</Link>
     </Layout>
